e28e8ae5452562a3d5dc0054f8ba9194872942f4
wppaing <wintphyopaing.p@gmail.com>
2022-01-18 12:52:53 +0630

    first

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+AOS.init();
+
+$(document).ready(function () {
+   $(".owl-carousel").owlCarousel({
+      items: 3,
+      autoplay: true,
+      autoplayTimeout: 3000,
+      dots: false,
+      loop: true,
+   });
+});
